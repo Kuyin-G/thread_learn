9903a8ba8b41db5ee286c6e3857404deedd245d7
Kuyin <kuyin457@163.com>
2021-05-26 20:23:11 +0800

    添加创建守护线程的方法和注释

--- src/main/java/thread01/learn/daemon/TestDaemonThread.java
+++ src/main/java/thread01/learn/daemon/TestDaemonThread.java
@@ -4,11 +4,19 @@ import org.junit.Test;
 
 public class TestDaemonThread {
 
+    /***
+     * 创建守护守护线程的方法
+     *      1、创建一个新的线程 thread
+     *      2、在线程启动前调用Thread实例的 thread.setDaemon(true)，设置线程为当前线程的守护线程
+     *      3、启动守护线程 -> thread.start();
+     *      在所在线程结束的时候，守护线程也会停止
+     * */
+
     @Test
     public void testDaemon() throws InterruptedException {
         Thread thread = new Thread(() -> {
             while (true) {
-                System.out.println("我是守护线程，正在执行监控任务...");
+                System.out.println("我是守护线程，正在执行守护任务...");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -21,6 +29,5 @@ public class TestDaemonThread {
         thread.start();
         Thread.sleep(4000);
         System.out.println("4秒后结束主线程，thread守护线程也会结束");
-
     }
 }
