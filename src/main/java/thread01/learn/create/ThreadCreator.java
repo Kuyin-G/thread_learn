6959e4c505abbc16b7b1b89efc9dad8c1c193d67
Kuyin <kuyin457@163.com>
2021-05-26 17:58:26 +0800

    创建多线程thread的方式：本质上都是实现Runnable的run()方法

--- src/main/java/thread01/learn/create/ThreadCreator.java
+++ src/main/java/thread01/learn/create/ThreadCreator.java
@@ -9,19 +9,32 @@ import java.util.concurrent.FutureTask;
 public class ThreadCreator {
 
     /**
-     * 测试使用继承或实现的方式
+     * 一、测试使用继承或实现的方式
+     *      1、继承Thread的方式： Thread 实现了 Runnable
+     *      2、现Runnable接口
+     *      3、Callable接口： FutureTask<V> 实现了 RunnableFuture<V> ->RunnableFuture<V>接口 继承了 Runnable
+     *   实质上全都是实现了Runnable的run方法
      * */
     @Test
     public void testExtendsOrImpl() throws ExecutionException, InterruptedException {
-
+        /**
+         * 1、通过继承Thread，重写run方法
+         * */
         ThreadExt threadExt = new ThreadExt();
         threadExt.start();
 
+        /**
+         * 2、通过实现Runnable接口，实现run方法，传递给Thread实例
+         * */
         Runnable runnable = new RunnableImpl();
         Thread thread = new Thread(runnable);
         thread.start();
 
-        Callable callable = new CallableImpl();
+        /**
+         * 2、通过实现Callable接口，实现call()方法，创建FutureTask实例，传递给Thread
+         * - 带返回只得线程
+         * */
+        Callable<String> callable = new CallableImpl();
         FutureTask<String> futureTask = new FutureTask<>(callable);
         Thread threadTask = new Thread(futureTask);
         threadTask.start();
@@ -30,7 +43,7 @@ public class ThreadCreator {
     }
 
     /**
-     *  如果只需要使用一次，可以使用匿名内部类
+     *   如果只需要使用一次，可以使用匿名内部类
      * */
     @Test
     public void testAnonymousClasses() throws ExecutionException, InterruptedException {
